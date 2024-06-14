6d867169e18cce3d117918534421165ecb784315
JerryUbuntu <jerryshum2636@gmail.com>
2024-06-14 16:56:51 -0400

    feat: finished star component

--- 010-usepopcorn/src/index.js
+++ 010-usepopcorn/src/index.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 import App from "./App";
 import StarRating from "./components/StarRating";
 
+function Test() {
+   const [movieRating, setMovieRating] = useState();
+
+   return (
+      <div>
+         <StarRating color="blue" onSetRating={setMovieRating} />
+         <p>This movie was rated {movieRating} stars.</p>
+      </div>
+   );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <React.StrictMode>
       {/* <App /> */}
-      <StarRating maxRating={5} />
+      <StarRating
+         maxRating={5}
+         messages={["Terrible", "Meh", "Decent", "Good", "Amazing!"]}
+         defaultRating={3}
+      />
+      <StarRating size={24} color="red" className="test" />
+
+      <Test></Test>
    </React.StrictMode>
 );
