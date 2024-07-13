9529a9bbdf9d88c6a2d125c3df09c76bd22891be
JerryUbuntu <jerryshum2636@gmail.com>
2024-07-12 20:27:15 -0400

    feat: finished 191
    - setup fake api server
    - used a useEffect hook to fetch data and catch errors on mount
    - used useReducer hook to set state for our application
    - setup initalState and reducer function for useReducer

--- 12-react-quiz/src/App.js
+++ 12-react-quiz/src/App.js
@@ -1,7 +1,48 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+   question: [],
+   // loading, error, ready, active, finished
+   status: "loading",
+};
+
+function reducer(state, action) {
+   switch (action.type) {
+      case "dataReceived":
+         return { ...state, question: action.payload, status: "ready" };
+      case "dataFailed":
+         return { ...state, status: "error" };
+      default:
+         throw new Error("Action Unknown");
+   }
+}
+
 export default function App() {
+   const [state, dispatch] = useReducer(reducer, initialState);
+
+   useEffect(function () {
+      async function fetchData() {
+         try {
+            const response = await fetch("http://localhost:9000/questions");
+
+            if (!response.ok) {
+               const errorMessage = `An error has occured: ${response.status}`;
+               throw new Error(errorMessage);
+            }
+
+            const quizQuestions = await response.json();
+            dispatch({ type: "dataReceived", payload: quizQuestions });
+         } catch (error) {
+            dispatch({ type: "dataFailed" });
+            console.log(error);
+         }
+      }
+
+      fetchData();
+   }, []);
+
    return (
       <div className="app">
          <Header />
