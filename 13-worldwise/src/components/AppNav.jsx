58bd758bd958823304f0d374ae3b0308f917d420
JerryUbuntu <jerryshum2636@gmail.com>
2024-07-19 20:31:37 -0400

    feat; working on 212

--- 13-worldwise/src/components/AppNav.jsx
+++ 13-worldwise/src/components/AppNav.jsx
@@ -1,7 +1,15 @@
 import styles from "./AppNav.module.css";
 
 function AppNav() {
-   return <nav className={styles.nav}>App Navigation</nav>;
+   return (
+      <nav className={styles.nav}>
+         <ul>
+            <li>
+               <NavLink></NavLink>
+            </li>
+         </ul>
+      </nav>
+   );
 }
 
 export default AppNav;
