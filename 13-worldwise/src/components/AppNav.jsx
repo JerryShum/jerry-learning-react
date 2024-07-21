37db0c4b38de8e774b4c2a9b0b7e7aaf22b12a20
JerryUbuntu <jerryshum2636@gmail.com>
2024-07-21 17:33:16 -0400

    feat: finished lecture 213 - implemented the cities list into the application - first setup states to handle the data returned from the api - used useEffect to make a response and fetch the api from our json-server - put the data into the cities state - used props to pass this array into the "cityList" component - conditionally rendering based on loading and if the array is empty - used .map() on the array to make a "CityItem" component for each city

--- 13-worldwise/src/components/AppNav.jsx
+++ 13-worldwise/src/components/AppNav.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
@@ -5,7 +6,10 @@ function AppNav() {
       <nav className={styles.nav}>
          <ul>
             <li>
-               <NavLink></NavLink>
+               <NavLink to="cities">Cities</NavLink>
+            </li>
+            <li>
+               <NavLink to="countries">Countries</NavLink>
             </li>
          </ul>
       </nav>
