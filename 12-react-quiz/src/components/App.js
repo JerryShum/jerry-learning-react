8bb8fff74e7d5fec477835af506bc9b405878568
JerryUbuntu <jerryshum2636@gmail.com>
2024-08-03 10:42:05 -0400

    committing from ubuntu

--- 12-react-quiz/src/components/App.js
+++ 12-react-quiz/src/components/App.js
@@ -11,66 +11,6 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
-const SECS_PER_QUESTION = 30;
-
-const initialState = {
-   questions: [],
-   // loading, error, ready, active, finished
-   status: "loading",
-   index: 0,
-   answer: null,
-   points: 0,
-   highscore: 0,
-   secondsRemaining: null,
-};
-
-function reducer(state, action) {
-   switch (action.type) {
-      case "dataReceived":
-         return { ...state, questions: action.payload, status: "ready" };
-      case "dataFailed":
-         return { ...state, status: "error" };
-      case "start":
-         return {
-            ...state,
-            status: "active",
-            secondsRemaining: state.questions.length * SECS_PER_QUESTION,
-         };
-      case "newAnswer":
-         const currentQuestion = state.questions.at(state.index);
-
-         return {
-            ...state,
-            answer: action.payload,
-            points:
-               action.payload === currentQuestion.correctOption
-                  ? state.points + currentQuestion.points
-                  : state.points,
-         };
-      case "nextQuestion":
-         return { ...state, index: state.index + 1, answer: null };
-      case "finish":
-         state.highscore = Math.max(state.points, state.highscore);
-         return { ...state, status: "finished" };
-      case "restart":
-         return {
-            ...initialState,
-            questions: state.questions,
-            status: "ready",
-            highscore: state.highscore,
-            secondsRemaining: 10,
-         };
-      case "tick":
-         return {
-            ...state,
-            secondsRemaining: state.secondsRemaining - 1,
-            status: state.secondsRemaining === 0 ? "finished" : state.status,
-         };
-      default:
-         throw new Error("Action Unknown");
-   }
-}
-
 export default function App() {
    useEffect(function () {
       async function fetchData() {
