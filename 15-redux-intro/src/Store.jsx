e42070c1f0a24fbd0ee87ffd5fa0461332f7833d
Jerry <jerryshum2636@gmail.com>
2024-08-09 13:26:08 -0400

    feat: completed 272
    - finished the section of old redux
    
    - learned the fundamentals

--- 15-redux-intro/src/Store.jsx
+++ 15-redux-intro/src/Store.jsx
@@ -1,13 +1,18 @@
 import { act } from "react";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
    account: accountReducer,
    customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+   rootReducer,
+   composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
