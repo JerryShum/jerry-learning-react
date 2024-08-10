fe677eed135bf59a8fad31e187608f9424e6fab0
Jerry <jerryshum2636@gmail.com>
2024-08-10 17:17:09 -0400

    feat: started to reconfigure the app using RTK

--- 15-redux-intro/src/Store.jsx
+++ 15-redux-intro/src/Store.jsx
@@ -1,18 +1,13 @@
-import { act } from "react";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { applyMiddleware, combineReducers, compose, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({
-   account: accountReducer,
-   customer: customerReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+   reducer: {
+      account: accountReducer,
+      customer: customerReducer,
+   },
 });
-
-const store = createStore(
-   rootReducer,
-   composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
