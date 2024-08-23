3938f2ff58f3490a8198ea62f1c8a0b0d448af39
Jerry <jerryshum2636@gmail.com>
2024-08-23 11:18:09 -0400

    finished 301 grid and flexbox

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Jerry</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jerry</div>;
 }
 
 export default Username;
