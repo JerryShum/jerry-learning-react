1bc85b45595d87d1da4d40f6eca17c320b51bad1
JerryShum <jerryshum2636@gmail.com>
2024-09-12 18:35:01 -0400

    feat: finished 309 styling the cart

--- 16-fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-3 py-1 md:py-2.5 md:px-5 text-xs',
+    secondary:
+      'inline-block rounded-full font-semibold border-stone-300 border uppercase tracking-wide text-stone-500 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-700 focus:bg-stone-300  focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3 md:px-6 md:py-4',
   };
 
   if (to) {
