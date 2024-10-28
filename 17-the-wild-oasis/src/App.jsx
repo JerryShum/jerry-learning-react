24aecc548b616e30d08b237e42499e0fa286c258
Jerry <jerryshum2636@gmail.com>
2024-10-28 16:23:18 -0400

    installed react icons and completed the general app layout

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Users from "./pages/Users";
+import Cabins from "./pages/Cabins";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
    return (
@@ -15,12 +17,15 @@ function App() {
          <GlobalStyles />
          <BrowserRouter>
             <Routes>
-               <Route index element={<Navigate replace to="dashboard" />} />
-               <Route path="dashboard" element={<Dashboard />} />
-               <Route path="bookings" element={<Bookings />} />
-               <Route path="cabins" element={<Users />} />
-               <Route path="settings" element={<Settings />} />
-               <Route path="account" element={<Account />} />
+               <Route element={<AppLayout />}>
+                  <Route index element={<Navigate replace to="dashboard" />} />
+                  <Route path="dashboard" element={<Dashboard />} />
+                  <Route path="bookings" element={<Bookings />} />
+                  <Route path="cabins" element={<Cabins />} />
+                  <Route path="users" element={<Users />} />
+                  <Route path="settings" element={<Settings />} />
+                  <Route path="account" element={<Account />} />
+               </Route>
                <Route path="login" element={<Login />} />
                <Route path="*" element={<PageNotFound />} />
             </Routes>
