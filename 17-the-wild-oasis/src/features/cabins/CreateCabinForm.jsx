639d02fcc5d268aa116d1db297df73ed2e52fcba
Jerry <jerryshum2636@gmail.com>
2024-11-02 20:49:51 -0400

    adding a modal window using react portal to cabins

--- 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit }) {
+function CreateCabinForm({ cabinToEdit, onCloseModal }) {
    const { id: editID, ...editValues } = cabinToEdit || {};
 
    // Checking to see if we're editing the cabin
@@ -44,7 +44,10 @@ function CreateCabinForm({ cabinToEdit }) {
          editCabin(
             { newCabinData: { ...data, image }, id: editID },
             {
-               onSuccess: (data) => reset(),
+               onSuccess: (data) => {
+                  reset();
+                  onCloseModal?.();
+               },
             }
          );
       }
@@ -55,7 +58,10 @@ function CreateCabinForm({ cabinToEdit }) {
    }
 
    return (
-      <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <Form
+         onSubmit={handleSubmit(onSubmit, onError)}
+         type={onCloseModal ? "modal" : "regular"}
+      >
          <FormRow label={"Cabin Name"} error={errors?.name?.message}>
             <Input
                type="text"
@@ -142,7 +148,12 @@ function CreateCabinForm({ cabinToEdit }) {
 
          <FormRow>
             {/* type is an HTML attribute! */}
-            <Button variation="secondary" type="reset" size="medium">
+            <Button
+               variation="secondary"
+               type="reset"
+               size="medium"
+               onClick={() => onCloseModal?.()}
+            >
                Cancel
             </Button>
             <Button variation="primary" size="medium" disabled={isWorking}>
