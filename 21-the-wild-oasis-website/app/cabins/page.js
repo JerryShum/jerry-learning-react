6aca6c0a7b11451cc6fa162c6ef4c109a86835e4
Jerry <jerryshum2636@gmail.com>
2024-11-16 15:56:13 -0500

    fetching data in a server component -> rendered in SC and then sent to client

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,5 +1,20 @@
-function Page() {
-   return <h1>This is the cabins page.</h1>;
+async function Page() {
+   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+   const data = await response.json();
+
+   console.log(data);
+
+   return (
+      <div>
+         <h1>This is the cabins page.</h1>
+
+         <ul>
+            {data.map((user) => (
+               <li key={user.id}>{user.name}</li>
+            ))}
+         </ul>
+      </div>
+   );
 }
 
 export default Page;
