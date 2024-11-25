6d8de8abc86b712a95e2d4dd7ea74a3d0baf8b19
Jerry <jerryshum2636@gmail.com>
2024-11-25 15:14:19 -0500

    Using the URL to share state between client and server.
    Building our client components -> as we click, we use next.js hooks to route to the URL.
    
    Back on the server, we add the searchparams to the page.
    
    Whenever the searchparams change, it re renders and passes it as props to the cabinlist

--- 21-the-wild-oasis-website/app/_components/CabinList.js
+++ 21-the-wild-oasis-website/app/_components/CabinList.js
@@ -2,20 +2,32 @@ import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
 import { unstable_noStore as noStore } from "next/cache";
 
-async function CabinList() {
+async function CabinList({ filter }) {
    // No store makes the data request NOT CACHED -> meaning the server doesn't store the result of the data fetched for this component
    noStore();
 
    // CHANGE
-   console.log("fetching cabins");
    const cabins = await getCabins();
    console.log(cabins);
+
+   let displayedCabins;
+   if (filter === "all") {
+      displayedCabins = cabins;
+   } else if (filter === "small") {
+      displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 4);
+   } else if (filter === "medium") {
+      displayedCabins = cabins.filter(
+         (cabin) => cabin.maxCapacity > 3 && cabin.maxCapacity <= 7
+      );
+   } else if (filter === "large") {
+      displayedCabins = cabins.filter((cabin) => cabin.maxCapacity > 8);
+   }
 
    if (!cabins.length) return null;
 
    return (
       <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-         {cabins.map((cabin) => (
+         {displayedCabins.map((cabin) => (
             <CabinCard cabin={cabin} key={cabin.id} />
          ))}
       </div>
--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
 
 // Revalidating and fetching new data every hour
 // export const revalidate = 3600;
@@ -11,7 +12,11 @@ export const metadata = {
    title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+   console.log(searchParams);
+
+   const filter = searchParams?.capacity ?? "all";
+
    return (
       <div>
          <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -26,8 +31,12 @@ export default async function Page() {
             peaceful, calm vacation. Welcome to paradise.
          </p>
 
-         <Suspense fallback={<Spinner />}>
-            <CabinList />
+         <div className="flex justify-end mb-8">
+            <Filter />
+         </div>
+
+         <Suspense fallback={<Spinner />} key={filter}>
+            <CabinList filter={filter} />
          </Suspense>
       </div>
    );
