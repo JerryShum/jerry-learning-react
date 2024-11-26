13b3bc32b58b42c4fa95d61d42c39315f633343e
Jerry <jerryshum2636@gmail.com>
2024-11-26 17:39:50 -0500

    authentication for users

--- 21-the-wild-oasis-website/app/_components/Navigation.js
+++ 21-the-wild-oasis-website/app/_components/Navigation.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
+export default async function Navigation() {
+   // Auth makes it so that the component is dynamic -> since navigation is on every route -> every route is dynamic?
+   const session = await auth();
+   console.log(session);
+
    return (
       <nav className="z-10 text-xl">
          <ul className="flex gap-16 items-center">
@@ -21,12 +27,30 @@ export default function Navigation() {
                </Link>
             </li>
             <li>
-               <Link
-                  href="/account"
-                  className="hover:text-accent-400 transition-colors"
-               >
-                  Guest area
-               </Link>
+               {session?.user?.image ? (
+                  <Link
+                     href="/account"
+                     className="hover:text-accent-400 transition-colors flex items-center gap-4"
+                  >
+                     <div className="h-8 w-8 relative rounded-full overflow-hidden">
+                        <Image
+                           src={session.user.image}
+                           className="rounded-full object-cover"
+                           fill
+                           alt={session.user.name}
+                           referrerPolicy="no-referrer"
+                        />
+                     </div>
+                     <span>Guest area</span>
+                  </Link>
+               ) : (
+                  <Link
+                     href="/account"
+                     className="hover:text-accent-400 transition-colors"
+                  >
+                     <span>Guest area</span>
+                  </Link>
+               )}
             </li>
          </ul>
       </nav>
