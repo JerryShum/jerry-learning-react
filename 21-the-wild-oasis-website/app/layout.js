516f06750fb34b2fa7944e6185b6f9baa0ffe86b
Jerry <jerryshum2636@gmail.com>
2024-11-26 15:29:31 -0500

    context api to share state between components

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -9,6 +9,7 @@ const josefin = Josefin_Sans({
    display: "swap",
 });
 
+import { ReservationProvider } from "@/app/_components/ReservationContext";
 import "@/app/_styles/globals.css";
 import Header from "./_components/Header";
 
@@ -33,7 +34,9 @@ function RootLayout({ children }) {
                <Navigation />
             </Header>
             <div className="flex-1 px-8 py-12 grid">
-               <main className="max-w-7xl mx-auto w-full">{children}</main>
+               <main className="max-w-7xl mx-auto w-full">
+                  <ReservationProvider>{children}</ReservationProvider>
+               </main>
             </div>
          </body>
       </html>
